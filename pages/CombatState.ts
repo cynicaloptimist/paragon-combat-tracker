6ca40813459e45dd53a4cc1ad8b76bb6ad06d2ad
Evan Bailey <cynicaloptimist@gmail.com>
2024-05-28 22:42:45 -0700

    Sort combatants by initiative

--- pages/CombatState.ts
+++ pages/CombatState.ts
@@ -1,3 +1,4 @@
+import _ from "lodash";
 import { Combatant, RulesPlugin } from "./RegisterPlugin";
 
 export type CombatState<TCharacter> = {
@@ -20,9 +21,21 @@ export const getDefaultCombatState: (
 ) => CombatState<any> = (rulesPlugin) => {
   const combatant = makeDefaultCombatant(rulesPlugin);
   const combatant2 = makeDefaultCombatant(rulesPlugin);
-
+  const combatant3 = makeDefaultCombatant(rulesPlugin);
+  combatant.initiativeResult = "10";
+  combatant2.initiativeResult = "4a";
+  combatant3.initiativeResult = "5a";
   return {
     activeCombatantIds: ["1"],
-    combatantsById: { "1": combatant, "2": combatant2 },
+    combatantsById: { "1": combatant, "2": combatant2, "3": combatant3 },
   };
+};
+
+export const sortCombatantsDefault = (
+  c1: Combatant<any>,
+  c2: Combatant<any>
+) => {
+  const i1 = _.parseInt(c1.initiativeResult);
+  const i2 = _.parseInt(c2.initiativeResult);
+  return i2 - i1;
 };
--- pages/components/Tracker.tsx
+++ pages/components/Tracker.tsx
@@ -1,6 +1,6 @@
 import { PropsWithChildren, useReducer } from "react";
 import { Combatant, RulesPlugin } from "../RegisterPlugin";
-import { getDefaultCombatState } from "../CombatState";
+import { getDefaultCombatState, sortCombatantsDefault } from "../CombatState";
 import { combatStateReducer } from "../combatStateReducer";
 
 type TrackerProps<TCharacter, TStatBlock> = {
@@ -11,21 +11,28 @@ export function Tracker<TCharacter, TStatBlock>(
   props: TrackerProps<TCharacter, TStatBlock>
 ) {
   const { rulesPlugin } = props;
-  const [state, dispatch] = useReducer(
-    combatStateReducer,
-    getDefaultCombatState(rulesPlugin)
-  );
+  // const [state, dispatch] = useReducer(
+  //   combatStateReducer,
+  //   getDefaultCombatState(rulesPlugin)
+  // );
+
+  const state = getDefaultCombatState(rulesPlugin);
 
   const activeCombatantId = state.activeCombatantIds[0];
   const activeCombatant = activeCombatantId
     ? state.combatantsById[activeCombatantId]
     : null;
 
+  const sortedCombatants = Object.entries(state.combatantsById).sort(
+    ([_k1, combatant1], [_k2, combatant2]) =>
+      sortCombatantsDefault(combatant1, combatant2)
+  );
+
   return (
     <div>
       <Heading>Initiative Order</Heading>
       <div>
-        {Object.entries(state.combatantsById).map(([key, combatant]) => {
+        {sortedCombatants.map(([key, combatant]) => {
           return (
             <div key={key}>{rulesPlugin.renderInitiativeRow(combatant)}</div>
           );
