1cc32f9c2ec929c2c90cf87156e808bc1da9c23e
Evan Bailey <cynicaloptimist@gmail.com>
2024-07-16 18:30:50 -0700

    Add pf2 plugin copied from dnd5e

--- src/pages/t/pf2.tsx
+++ src/pages/t/pf2.tsx
@@ -1,11 +1,13 @@
 import Head from "next/head";
+import { Tracker } from "~/components/Tracker";
+import { pf2 } from "~/plugins/pf2.plugin";
 
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { NextPageContext } from "next";
-import { Button } from "~/components/Button";
 
 export async function getStaticProps({ locale }: NextPageContext) {
   return {
+    
     props: {
       ...(await serverSideTranslations(locale || "en", ["common"])),
       // Will be passed to the page component as props
@@ -21,13 +23,8 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <div className="m-2 md:m-8 flex flex-col items-center">
-          <div className="gap-2 flex flex-col w-80">
-            <Button href="/t/pf2">Pathfinder 2</Button>
-            <Button href="/t/dnd5e">D&D 5e</Button>
-          </div>
-        </div>
+      <main className="h-full">
+        <Tracker rulesPlugin={pf2} />
       </main>
     </>
   );
---
+++ src/plugins/pf2.plugin.tsx
@@ -0,0 +1,147 @@
+import _ from "lodash";
+import { PromptComponentProps } from "~/plugin-utils/RulesPlugin";
+import { RulesPlugin } from "~/plugin-utils/RulesPlugin";
+import { useCallback, useEffect, useRef } from "react";
+import { Button } from "~/components/Button";
+import { TextInput } from "~/components/TextInput";
+import { z } from "zod";
+
+export type Pf2Character = {
+  statBlock: Pf2StatBlock;
+  currentHP: number;
+};
+
+const traitSchema = z.object({
+  name: z.string().optional(),
+  description: z.string().optional(),
+  usage: z.string().optional(),
+});
+
+const pf2StatBlockSchema = z.object({
+  initiativeModifier: z.number().default(0),
+  name: z.string().default("Default StatBlock"),
+  size: z.string().default("Medium"),
+  type: z.string().default(""),
+  alignment: z.string().default(""),
+  maxHP: z.number().default(10),
+  hitDice: z.string().default(""),
+  armorClass: z.number().default(10),
+  armorClassNotes: z.string().default(""),
+  speeds: z.array(z.string()).default([]),
+  abilityScores: z
+    .object({
+      strength: z.number().default(10),
+      dexterity: z.number().default(10),
+      constitution: z.number().default(10),
+      intelligence: z.number().default(10),
+      wisdom: z.number().default(10),
+      charisma: z.number().default(10),
+    })
+    .default({}),
+  savingThrows: z
+    .object({
+      strength: z.number().optional(),
+      dexterity: z.number().optional(),
+      constitution: z.number().optional(),
+      intelligence: z.number().optional(),
+      wisdom: z.number().optional(),
+      charisma: z.number().optional(),
+    })
+    .default({}),
+  skills: z.record(z.number()).default({}),
+  senses: z.array(z.string()).default([]),
+  languages: z.array(z.string()).default([]),
+  challengeRating: z.string().default("0"),
+  proficiencyBonus: z.number().default(2),
+  traits: z.array(traitSchema).default([]),
+  actions: z.array(traitSchema).default([]),
+  reactions: z.array(traitSchema).default([]),
+  bonusActions: z.array(traitSchema).default([]),
+  legendaryActions: z.array(traitSchema).default([]),
+});
+
+export type Pf2StatBlock = z.infer<typeof pf2StatBlockSchema>;
+
+function DefaultStatBlock(): Pf2StatBlock {
+  return pf2StatBlockSchema.parse({});
+}
+
+export const pf2: RulesPlugin<Pf2Character, Pf2StatBlock> = {
+  loadStatBlock(inputString) {
+    return DefaultStatBlock();
+  },
+  loadCharacter(inputString) {
+    return {
+      statBlock: DefaultStatBlock(),
+      currentHP: DefaultStatBlock().maxHP,
+    };
+  },
+  getInitiativeResult(combatant) {
+    return combatant.character.statBlock.initiativeModifier.toString();
+  },
+  initializeCharacter(statBlock) {
+    return {
+      statBlock,
+      currentHP: statBlock.maxHP,
+    };
+  },
+  renderFullView(combatant) {
+    return (
+      <pre className="overflow-x-auto">
+        {JSON.stringify(combatant, null, 2)}
+      </pre>
+    );
+  },
+  renderSmallView(combatant) {
+    return (
+      <pre className="overflow-x-auto">
+        {JSON.stringify(combatant, null, 2)}
+      </pre>
+    );
+  },
+  renderInitiativeRow(combatant) {
+    const { character, initiativeResult } = combatant;
+    const { statBlock, currentHP } = character;
+    const { name, maxHP } = statBlock;
+    return (
+      <div className="flex flex-row gap-2 py-2">
+        <div className="font-bold text-right w-6">{initiativeResult}</div>
+        <div>{name}</div>
+        <div>
+          {currentHP}/{maxHP}
+        </div>
+      </div>
+    );
+  },
+  getCombatantCommands() {
+    return [
+      {
+        label: "Apply Damage",
+        prompt: ApplyDamagePrompt,
+      },
+    ];
+  },
+};
+
+const ApplyDamagePrompt = (props: PromptComponentProps<Pf2Character>) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  useEffect(() => {
+    inputRef.current?.focus();
+  });
+
+  const onSubmit = useCallback(() => {
+    const damageAmount = inputRef.current?.value;
+    const newHp =
+      props.combatant.character.currentHP - _.parseInt(damageAmount || "0");
+    props.combatant.character.currentHP = newHp;
+    props.updateCombatant(props.combatant);
+    props.complete();
+  }, [props]);
+
+  return (
+    <div className="flex flex-row items-center gap-2">
+      <TextInput itemRef={inputRef} label="Apply Damage" onEnter={onSubmit} />
+      <Button onClick={onSubmit}>Submit</Button>
+    </div>
+  );
+};
